b6484653f8cca3e67eeab2fa36e5908dae67d921
parzivail <colby.newman2000@gmail.com>
2019-04-29 23:37:45 -0400

    reference ScarifLib

--- Worldshape/Program.cs
+++ Worldshape/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScarifLib;
 
 namespace Worldshape
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
+            if (args.Length == 0)
+                return -1;
+
+            var structure = ScarifStructure.Load(args[0]);
+
+            Console.ReadKey();
+
+            return 0;
         }
     }
 }
