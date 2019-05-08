ec06234fe983d8a94f0414a17d6763da591bfc1b
parzivail <colby.newman2000@gmail.com>
2019-05-08 11:16:42 -0400

    added blockmap config file classes

---
+++ Worldshape/Configuration/BlockMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Worldshape.Configuration
+{
+	class BlockMap
+	{
+		[JsonProperty("minecraft", Required = Required.Always)]
+		public string MinecraftVersion { get; set; }
+
+		[JsonProperty("textureDir", Required = Required.Always)]
+		public string RootTextureDir { get; set; }
+
+		[JsonProperty("entries", Required = Required.Always)]
+		public List<BlockMapping> Entries { get; set; }
+	}
+}
---
+++ Worldshape/Configuration/BlockMapping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Worldshape.Configuration
+{
+	internal class BlockMapping
+	{
+		[JsonProperty("name", Required = Required.Always)]
+		public string Name { get; set; }
+
+		[JsonProperty("id", Required = Required.Always)]
+		public int Id { get; set; }
+
+		[JsonProperty("maxMeta", Required = Required.Always)]
+		public int MaxMetadata { get; set; }
+
+		[JsonProperty("render")]
+		public string Render { get; set; }
+
+		[JsonProperty("tint")]
+		public bool UseBiomeTint { get; set; }
+
+		[JsonProperty("texture", Required = Required.Always)]
+		public List<string> Texture { get; set; }
+	}
+}
